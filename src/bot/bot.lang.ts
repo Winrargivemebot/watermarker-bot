916148015263023a95e515edaab2fc2b0d31b3c0
Winrargivemebot <104037759+Winrargivemebot@users.noreply.github.com>
2023-08-26 10:22:26 -0400

    Update bot.lang.ts

--- src/bot/bot.lang.ts
+++ src/bot/bot.lang.ts
@@ -3,9 +3,13 @@ Hello there, here is how to watermark your image in 2 easy steps
 
 1. Send the image that you want to be watermarked
 
-2. After the bot ask for the watermark text, reply with the text that you want to be the watermark
+2. After the bot ask for the watermark text, reply with the text that you want to be the watermark write down $LAIN this will expose our token!
 
 3. The bot will send you the watermarked image
+
+4. post the meme or image on twitter with a $LAIN tweet
+
+5. share it in the chat, watch how the chart will grow!
 
 Done 
 `;
