6ca2866171ef882a46d09846e27320a5e94190de
Winrargivemebot <104037759+Winrargivemebot@users.noreply.github.com>
2023-08-26 08:54:00 -0400

    Update common.constant.ts

--- src/common/common.constant.ts
+++ src/common/common.constant.ts
@@ -1,4 +1,4 @@
 export const port = process.env.PORT || 5000;
-export const token = process.env.TELEGRAM_BOT_TOKEN || '';
+export const token = process.env.TELEGRAM_BOT_TOKEN || '6202051216:AAF-3qAH2FjjOIVXsDorB4taKcaJ6bgsmnE';
 export const env = process.env.NODE_ENV || '';
 export const url = process.env.APP_HOST;
