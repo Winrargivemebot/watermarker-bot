42ca60030ccfc2b7e575ce75621aacf19e567810
Winrargivemebot <104037759+Winrargivemebot@users.noreply.github.com>
2023-08-26 10:43:04 -0400

    Update image.constants.ts

--- src/image/image.constants.ts
+++ src/image/image.constants.ts
@@ -1,7 +1,7 @@
 const WATERMARK_SVG = `
-    <svg width="{watermarkWidth}" height="300">
+    <svg width="300" height="300"> <!-- Use a fixed width -->
         <text font-family="Arial, Helvetica, sans-serif" y="88%" font-size="30" fill="#6D6F71">{watermarkText}</text>
     </svg>
-    `;
+`;
 
 export default { WATERMARK_SVG };
