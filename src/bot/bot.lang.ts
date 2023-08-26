340804b17350fbb9895327ef116f887f9b8ebf44
Winrargivemebot <104037759+Winrargivemebot@users.noreply.github.com>
2023-08-26 11:33:32 -0400

    Update bot.lang.ts

--- src/bot/bot.lang.ts
+++ src/bot/bot.lang.ts
@@ -1,15 +1,11 @@
 export const howToMessage = `
-Hello there, here is how to watermark your image in 2 easy steps
+1. Send the image 
 
-1. Send the image that you want to be watermarked
+2.. The bot will send you the $LAIN image
 
-2. After the bot ask for the watermark text, reply with the text that you want to be the watermark write down $LAIN this will expose our token!
+3. post the meme or image on twitter with a $LAIN tweet
 
-3. The bot will send you the watermarked image
-
-4. post the meme or image on twitter with a $LAIN tweet
-
-5. share it in the chat, watch how the chart will grow!
+4. share the tweet link in the chat retweeting and liking, watch how the chart will grow!
 
 Done 
 `;
@@ -20,6 +16,5 @@ deployed by @Usernamei234567891011121314151
 
 export const startMessage = `
 Hello {name}, nice to meet you!
-
-Send '/howto' to see information on how to use this bot
+send image to start or type /start
 `;
