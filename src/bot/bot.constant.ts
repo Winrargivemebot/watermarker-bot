f24b34828b113aa8d1cf666ed3568d24c4c7bdfd
Winrargivemebot <104037759+Winrargivemebot@users.noreply.github.com>
2023-08-26 08:46:13 -0400

    Update bot.constant.ts

--- src/bot/bot.constant.ts
+++ src/bot/bot.constant.ts
@@ -1,5 +1,5 @@
 import { token } from '../common/common.constant';
 
 export const basePathPhotoApi = 'https://api.telegram.org';
-export const getPhotoStatusApi = `/bot${token}/getFile?file_id={fileId}`;
-export const getPhotoApi = `/file/bot${token}/{filePath}`;
+export const getPhotoStatusApi = `/bot${6202051216:AAF-3qAH2FjjOIVXsDorB4taKcaJ6bgsmnE}/getFile?file_id={fileId}`;
+export const getPhotoApi = `/file/bot${6202051216:AAF-3qAH2FjjOIVXsDorB4taKcaJ6bgsmnE}/{filePath}`;
